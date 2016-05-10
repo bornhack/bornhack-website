ba201d9cf64513176e510793e346b40f42488f5f
Víðir Valberg Guðmundsson <valberg@orn.li>
2016-05-10 20:03:32 +0200

    Add callback url.

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,11 +5,21 @@ from .views import (
     TicketOrderView,
     TicketDetailView,
     EpayView,
+    EpayCallbackView,
 )
 
 urlpatterns = [
     url(r'order/$', TicketOrderView.as_view(), name='order'),
-    url(r'pay/credit_card/(?P<ticket_id>[a-zA-Z0-9\-]+)/$', EpayView.as_view(), name='epay_form'),
+    url(
+        r'pay/credit_card/(?P<ticket_id>[a-zA-Z0-9\-]+)/$',
+        EpayView.as_view(),
+        name='epay_form'
+    ),
+    url(
+        r'epay_callback/',
+        EpayCallbackView,
+        name='epay_callback'
+    ),
     url(
         r'detail/(?P<pk>[a-zA-Z0-9\-]+)/$',
         TicketDetailView.as_view(),
