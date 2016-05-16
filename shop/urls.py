e8badb817ac162db1284312ccc0bbb6908719fe4
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-05-16 18:26:27 +0200

    use DetailView, add epay_thanks, unfinished

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'orders/$', OrderListView.as_view(), name='order_list'),
     url(r'orders/(?P<pk>[0-9]+)/$', OrderDetailView.as_view(), name='order_detail'),
     url(r'orders/(?P<pk>[0-9]+)/pay/creditcard/$', EpayFormView.as_view(), name='epay_form'),
+    url(r'orders/(?P<pk>[0-9]+)/pay/creditcard/thanks/$', EpayThanksView.as_view(), name='epay_thanks'),
     url(r'orders/(?P<pk>[0-9]+)/pay/blockchain/$', CoinifyRedirectView.as_view(), name='coinify_pay'),
     url(r'orders/(?P<pk>[0-9]+)/pay/banktransfer/$', BankTransferView.as_view(), name='bank_transfer'),
     url(r'epay_callback/', EpayCallbackView.as_view(), name='epay_callback'),
