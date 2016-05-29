93fed32cad0fadf9d2310eb126e5f14d3a560b52
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-05-29 15:16:29 +0200

    styling order list page

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -8,3 +8,10 @@ def currency(value):
     return "{0:.2f} DKK".format(value)
 
 
+@register.filter(is_safe=True)
+def truefalseicon(value):
+    if value:
+        return "<span class='text-success'>{% bootstrap_icon 'ok' %}</span>"
+    else:
+        return "<span class='text-danger'>{% bootstrap_icon 'remove' %}</span>"
+
