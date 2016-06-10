ef00c130f9fa507a36b7f99b365f17dd314b2742
Víðir Valberg Guðmundsson <valberg@orn.li>
2016-06-10 20:21:32 +0200

    Make publication depend on 'published_at' field

--- news/managers.py
+++ news/managers.py
@@ -1,9 +1,11 @@
 from django.db.models import QuerySet
+from django.utils import timezone
 
 
 class NewsItemQuerySet(QuerySet):
 
     def public(self):
         return self.filter(
-            public=True
+            public=True,
+            published_at__lt=timezone.now()
         )
--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
 from . import models
 
 
@@ -13,4 +14,12 @@ class NewsDetail(DetailView):
     model = models.NewsItem
     template_name = 'news_detail.html'
     context_object_name = 'news_item'
+
+    def get_context_data(self, **kwargs):
+        context = super(NewsDetail, self).get_context_data(**kwargs)
+        news_item = self.get_object()
+        context['draft'] = False
+        if news_item.public and news_item.published_at > timezone.now():
+            context['draft'] = True
+        return context
 
