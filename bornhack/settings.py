820000ca8359b5a61cde7d2b3dc28dc342700410
Thomas Steen Rasmussen <tykling@bornhack.org>
2016-11-22 21:04:06 +0100

    make settings, env settings, and requirements less complex

--- bornhack/settings.py
+++ bornhack/settings.py
@@ -48,6 +48,42 @@ INSTALLED_APPS = [
     'bootstrap3',
 ]
 
+DEBUG = env('DEBUG')
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    INSTALLED_APPS += ['debug_toolbar', ]
+else:
+    EMAIL_HOST = env('EMAIL_HOST')
+    EMAIL_PORT = env('EMAIL_PORT')
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+    DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+    SERVER_EMAIL = env('DEFAULT_FROM_EMAIL')
+    ARCHIVE_EMAIL = env('ARCHIVE_EMAIL')
+
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler',
+            },
+            'console': {
+                'level':'DEBUG',
+                'class':'logging.StreamHandler',
+            },
+        },
+        'loggers': {
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+        }
+    }
+
 STATIC_URL = '/static/'
 STATIC_ROOT = local_dir('static')
 STATICFILES_DIRS = [local_dir('static_src')]
