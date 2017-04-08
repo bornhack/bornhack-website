1bb2b8d9a9226c77a0908db2d9abe7b787f01e56
Thomas Steen Rasmussen <tykling@bornhack.org>
2017-04-08 09:39:43 +0200

    a bit more work on the team admin

--- src/teams/admin.py
+++ src/teams/admin.py
@@ -6,8 +6,20 @@ admin.site.register(TeamArea)
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
+    def get_responsible(self, obj):
+        return ", ".join([resp.get_full_name() for resp in obj.responsible])
+    get_responsible.short_description = 'Responsible'
+
+    list_display = [
+        'name',
+        'area',
+        'get_responsible',
+        'needs_members',
+    ]
+
     list_filter = [
         'camp',
+        'needs_members',
     ]
 
 @admin.register(TeamMember)
