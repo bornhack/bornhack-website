3cde4af189269c326c43d049abdb004b33d82629
Thomas Steen Rasmussen <tykling@bornhack.org>
2017-08-20 00:44:55 +0200

    syntax

--- src/tickets/views.py
+++ src/tickets/views.py
@@ -41,7 +41,7 @@ class ShopTicketDownloadView(LoginRequiredMixin, SingleObjectMixin, View):
     def get(self, request, *args, **kwargs):
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="{type}_ticket_{pk}.pdf"'.format(
-            type=self.get_object().shortname
+            type=self.get_object().shortname,
             pk=self.get_object().pk
         )
         response.write(self.get_object().generate_pdf().getvalue())
