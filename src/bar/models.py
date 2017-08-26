ee4c0819e41379722d67214ff5a8f4ff331854e7
Vidir Valberg Gudmundsson <valberg@orn.li>
2017-08-26 20:28:11 +0200

    Order categories and products alphabetically.

--- src/bar/models.py
+++ src/bar/models.py
@@ -10,12 +10,18 @@ class ProductCategory(CampRelatedModel):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ("name",)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     category = models.ForeignKey(ProductCategory, related_name="products")
     in_stock = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("name",)
 
     def __str__(self):
         return self.name
