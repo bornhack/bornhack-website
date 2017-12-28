456ffa1bead791be9e5bbdc2642d4648b292bbf6
Jeppe Ernst <impala.lowrider@gmail.com>
2017-12-28 21:50:48 +0100

    useless/redundant file
    forgot to remove it in the previous commit

--- src/bornhack/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.decorators import login_required
-
-# Ensure users go through the allauth workflow when logging into admin.
-admin.site.login = login_required(admin.site.login)
-# Run the standard admin set-up.
-admin.autodiscover()
