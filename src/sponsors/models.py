d140155b6331409a315890b7d58e9e2a6fde552d
Thomas Steen Rasmussen <tykling@bornhack.org>
2018-03-04 14:41:51 +0100

    remove unneeded imports from sponsors app

--- src/sponsors/models.py
+++ src/sponsors/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from utils.models import CampRelatedModel, CreatedUpdatedModel
+from utils.models import CampRelatedModel
 
 
 def get_sponsor_upload_path(instance, filename):
