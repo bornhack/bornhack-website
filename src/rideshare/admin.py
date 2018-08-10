c83ebfed082883a4d20175dab4b74883b0e08f43
Víðir Valberg Guðmundsson <valberg@orn.li>
2018-08-10 18:37:27 +0200

    Adding admin for rideshare.Ride

--- src/rideshare/admin.py
+++ src/rideshare/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ride
+
+
+@admin.register(Ride)
+class RideModelAdmin(admin.ModelAdmin):
+    list_filter = ('camp', 'user')
