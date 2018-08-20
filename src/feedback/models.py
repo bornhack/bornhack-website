98433be3651b29908f384240e2c8f67fa7863123
Víðir Valberg Guðmundsson <valberg@orn.li>
2018-08-20 15:52:11 +0200

    Should be a CampRelatedModel.

--- src/feedback/models.py
+++ src/feedback/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-from utils.models import UUIDModel, CreatedUpdatedModel
+from utils.models import UUIDModel, CreatedUpdatedModel, CampRelatedModel
 
 
-class Feedback(UUIDModel, CreatedUpdatedModel):
+class Feedback(CampRelatedModel, UUIDModel):
+    camp = models.ForeignKey('camps.Camp', on_delete=models.PROTECT)
     user = models.ForeignKey('auth.User', on_delete=models.PROTECT)
     feedback = models.TextField()
