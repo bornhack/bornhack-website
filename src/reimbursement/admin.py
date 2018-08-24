510c71a6926ea7fbe971a43396a2a2117d4d7daa
Víðir Valberg Guðmundsson <valberg@orn.li>
2018-08-24 21:57:31 +0200

    Add a way to approve/reject an reimbursement and send mails accordingly.

--- src/reimbursement/admin.py
+++ src/reimbursement/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 from reimbursement.models import Reimbursement
 
 
+def approve_reimbursements(modeladmin, request, queryset):
+    for reimbursement in queryset.all():
+        reimbursement.approve()
+approve_reimbursements.short_description = "Approve"
+
+
+def reject_reimbursements(modeladmin, request, queryset):
+    for reimbursement in queryset.all():
+        reimbursement.reject()
+reject_reimbursements.short_description = "Reject"
+
+
 @admin.register(Reimbursement)
 class ReimbursementAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['user', 'amount', 'description', 'camp']
+    list_filter = ['camp', 'approved']
+    actions = [approve_reimbursements, reject_reimbursements]
