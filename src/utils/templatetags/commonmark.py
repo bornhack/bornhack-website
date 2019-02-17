3a84dd62d2d03122b6fcb175e6e68d82d978697b
Thomas Steen Rasmussen <tykling@bornhack.org>
2019-02-17 17:39:42 +0100

    use bleach.linkify to make links in markdown clickable automagically, fixes #64

--- src/utils/templatetags/commonmark.py
+++ src/utils/templatetags/commonmark.py
@@ -1,4 +1,5 @@
 import commonmark, bleach
+from html5lib.tokenizer import HTMLTokenizer
 
 from django import template
 from django.utils.safestring import mark_safe
@@ -7,21 +8,22 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 
-@register.filter
+@register.filter(is_safe=True)
 @stringfilter
 def trustedcommonmark(value):
     """Returns HTML given some commonmark Markdown. Also allows real HTML, so do not use this with untrusted input."""
     parser = commonmark.Parser()
     renderer = commonmark.HtmlRenderer()
     ast = parser.parse(value)
-    return mark_safe(renderer.render(ast))
+    return bleach.linkify(renderer.render(ast), skip_pre=True, parse_email=True, tokenizer=HTMLTokenizer)
 
-@register.filter
+
+@register.filter(is_safe=True)
 @stringfilter
 def untrustedcommonmark(value):
     """Returns HTML given some commonmark Markdown. Cleans actual HTML from input using bleach, suitable for use with untrusted input."""
     parser = commonmark.Parser()
     renderer = commonmark.HtmlRenderer()
     ast = parser.parse(bleach.clean(value))
-    return mark_safe(renderer.render(ast))
+    return bleach.linkify(renderer.render(ast), skip_pre=True, parse_email=True, tokenizer=HTMLTokenizer)
 
