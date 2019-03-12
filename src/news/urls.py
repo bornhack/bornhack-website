92a5f67ae31c7746b420dfda70c095779b89cb46
Víðir Valberg Guðmundsson <valberg@orn.li>
2019-03-12 10:13:27 +0100

    Add RSS feed to news.

--- src/news/urls.py
+++ src/news/urls.py
@@ -5,6 +5,7 @@ app_name = 'news'
 urlpatterns = [
     path('', views.NewsIndex.as_view(), kwargs={'archived': False}, name='index'),
     path('archive/', views.NewsIndex.as_view(), kwargs={'archived': True}, name='archive'),
+    path('feed/', views.NewsFeed(), name='feed'),
     path('<slug:slug>/', views.NewsDetail.as_view(), name='detail'),
 ]
 
