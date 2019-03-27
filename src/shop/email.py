8a5b2e5ed1f7d7152d8f174e94bbc8796e5c2574
Víðir Valberg Guðmundsson <valberg@orn.li>
2019-03-27 13:49:46 +0100

    Add a worker to cancel old orders.

--- src/shop/email.py
+++ src/shop/email.py
@@ -1,5 +1,9 @@
-from utils.email import add_outgoing_email
 import logging
+
+from django.conf import settings
+
+from utils.email import add_outgoing_email
+
 logger = logging.getLogger("bornhack.%s" % __name__)
 
 
@@ -42,6 +46,25 @@ def add_invoice_email(invoice):
         subject=subject,
         attachment=invoice.pdf.read(),
         attachment_filename=invoice.filename
+    )
+
+
+def add_order_cancelled_email(order):
+    formatdict = {
+        'ordernumber': order.pk,
+        'order_ttl': settings.ORDER_TTL,
+        'order_ttl_unit': settings.ORDER_TTL_UNIT
+    }
+
+    subject = 'Your non-paid BornHack order has been cancelled.'
+
+    # add email to outgoing email queue
+    return add_outgoing_email(
+        text_template='emails/order_cancelled_email.txt',
+        html_template='emails/order_cancelled_email.html',
+        to_recipients=order.user.email,
+        formatdict=formatdict,
+        subject=subject,
     )
 
 
