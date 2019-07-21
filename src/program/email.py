d2760323897c732a06f98c43473ba04510783319
Thomas Steen Rasmussen <tykling@bornhack.org>
2019-07-21 15:26:57 +0200

    better subjects for content team emails, and add quotes in body

--- src/program/email.py
+++ src/program/email.py
@@ -12,70 +12,66 @@ def add_new_speakerproposal_email(speakerproposal):
 
     try:
         content_team = Team.objects.get(camp=speakerproposal.camp, name="Content")
-
-        return add_outgoing_email(
-            text_template="emails/new_speakerproposal.txt",
-            html_template="emails/new_speakerproposal.html",
-            to_recipients=content_team.mailing_list,
-            formatdict=formatdict,
-            subject="New speaker proposal for {}".format(speakerproposal.camp.title),
-        )
     except ObjectDoesNotExist as e:
         logger.info("There is no team with name Content: {}".format(e))
         return False
 
+    return add_outgoing_email(
+        text_template="emails/new_speakerproposal.txt",
+        html_template="emails/new_speakerproposal.html",
+        to_recipients=content_team.mailing_list,
+        formatdict=formatdict,
+        subject="New speaker proposal '%s' was just submitted" % speakerproposal.name,
+    )
 
 def add_new_eventproposal_email(eventproposal):
     formatdict = {"proposal": eventproposal}
 
     try:
         content_team = Team.objects.get(camp=eventproposal.camp, name="Content")
-
-        return add_outgoing_email(
-            text_template="emails/new_eventproposal.txt",
-            html_template="emails/new_eventproposal.html",
-            to_recipients=content_team.mailing_list,
-            formatdict=formatdict,
-            subject="New event proposal for {}".format(eventproposal.camp.title),
-        )
     except ObjectDoesNotExist as e:
         logger.info("There is no team with name Content: {}".format(e))
         return False
 
+    return add_outgoing_email(
+        text_template="emails/new_eventproposal.txt",
+        html_template="emails/new_eventproposal.html",
+        to_recipients=content_team.mailing_list,
+        formatdict=formatdict,
+        subject="New event proposal '%s' was just submitted" % eventproposal.title,
+    )
 
 def add_speakerproposal_updated_email(speakerproposal):
     formatdict = {"proposal": speakerproposal}
 
     try:
         content_team = Team.objects.get(camp=speakerproposal.camp, name="Content")
-
-        return add_outgoing_email(
-            text_template="emails/update_speakerproposal.txt",
-            html_template="emails/update_speakerproposal.html",
-            to_recipients=content_team.mailing_list,
-            formatdict=formatdict,
-            subject="Updated speaker proposal for {}".format(
-                speakerproposal.camp.title
-            ),
-        )
     except ObjectDoesNotExist as e:
         logger.info("There is no team with name Content: {}".format(e))
         return False
 
+    return add_outgoing_email(
+        text_template="emails/update_speakerproposal.txt",
+        html_template="emails/update_speakerproposal.html",
+        to_recipients=content_team.mailing_list,
+        formatdict=formatdict,
+        subject="Speaker proposal '%s' was just updated" % speakerproposal.name,
+    )
 
 def add_eventproposal_updated_email(eventproposal):
     formatdict = {"proposal": eventproposal}
 
     try:
         content_team = Team.objects.get(camp=eventproposal.camp, name="Content")
-
-        return add_outgoing_email(
-            text_template="emails/update_eventproposal.txt",
-            html_template="emails/update_eventproposal.html",
-            to_recipients=content_team.mailing_list,
-            formatdict=formatdict,
-            subject="New event proposal for {}".format(eventproposal.camp.title),
-        )
     except ObjectDoesNotExist as e:
         logger.info("There is no team with name Content: {}".format(e))
         return False
+
+    return add_outgoing_email(
+        text_template="emails/update_eventproposal.txt",
+        html_template="emails/update_eventproposal.html",
+        to_recipients=content_team.mailing_list,
+        formatdict=formatdict,
+        subject="Event proposal '%s' was just updated" % eventproposal.title,
+    )
+
