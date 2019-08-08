17ff6efb8fa4674b4970a9e04c369d724cb459db
Víðir Valberg Guðmundsson <valberg@orn.li>
2019-08-08 11:11:17 +0200

    Use list_display instead.

--- src/utils/admin.py
+++ src/utils/admin.py
@@ -6,4 +6,4 @@ from .models import OutgoingEmail
 @admin.register(OutgoingEmail)
 class OutgoingEmailAdmin(admin.ModelAdmin):
     model = OutgoingEmail
-    fields = ['subject', 'to_recipients']
+    list_display = ['subject', 'to_recipients']
