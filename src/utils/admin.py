57dc6f12d831756ce257b9917803bd7301d1eff7
Thomas Steen Rasmussen <tykling@bornhack.org>
2020-07-29 21:07:50 +0200

    add admin actions to hold and release emails

--- src/utils/admin.py
+++ src/utils/admin.py
@@ -8,3 +8,18 @@ class OutgoingEmailAdmin(admin.ModelAdmin):
     model = OutgoingEmail
     list_display = ["subject", "to_recipients", "processed", "hold", "responsible_team"]
     list_filter = ["processed", "hold", "responsible_team"]
+    actions = ["hold_emails", "release_emails"]
+
+    def hold_emails(self, request, queryset):
+        for email in queryset.filter(hold=False, processed=False):
+            email.hold = True
+            email.save()
+
+    hold_emails.description = "Mark these emails with hold=True"
+
+    def release_emails(self, request, queryset):
+        for email in queryset.filter(hold=True, processed=False):
+            email.hold = False
+            email.save()
+
+    release_emails.description = "Mark these emails with hold=False"
