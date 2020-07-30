51c95ddc9dfb8527df8849c027706815f960528e
Thomas Steen Rasmussen <tykling@bornhack.org>
2020-07-30 20:45:35 +0200

    how did this survive for so long!

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -5,5 +5,5 @@ from .views import ProfileDetail, ProfileUpdate
 app_name = "profiles"
 urlpatterns = [
     path("", ProfileDetail.as_view(), name="detail"),
-    path("edit", ProfileUpdate.as_view(), name="update"),
+    path("edit/", ProfileUpdate.as_view(), name="update"),
 ]
