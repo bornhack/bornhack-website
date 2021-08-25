bba67c7dbea48c4ff79379749056cdfe35273d6c
Thomas Steen Rasmussen <tykling@bornhack.org>
2021-08-25 02:19:28 +0200

    needs moderation

--- src/villages/urls.py
+++ src/villages/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from .views import (
-    VillageCreateView,
     VillageDeleteView,
     VillageDetailView,
     VillageListView,
@@ -12,7 +11,7 @@ app_name = "villages"
 
 urlpatterns = [
     path("", VillageListView.as_view(), name="list"),
-    path("create/", VillageCreateView.as_view(), name="create"),
+    # path("create/", VillageCreateView.as_view(), name="create"),
     path("<slug:slug>/delete/", VillageDeleteView.as_view(), name="delete"),
     path("<slug:slug>/edit/", VillageUpdateView.as_view(), name="update"),
     path("<slug:slug>/", VillageDetailView.as_view(), name="detail"),
