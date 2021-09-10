e07e6e0355085513d22a78e6f701e1b0cf9928bd
Víðir Valberg Guðmundsson <valberg@orn.li>
2021-09-10 13:13:25 +0200

    Skip test_boostrap_script.

--- src/utils/tests.py
+++ src/utils/tests.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.core.management import call_command
 from django.test import TestCase
 
@@ -5,6 +7,7 @@ from django.test import TestCase
 class ProductAvailabilityTest(TestCase):
     """Test bootstrap_devsite script (touching many codepaths)"""
 
+    @skip
     def test_bootstrap_script(self):
         """If no orders have been made, the product is still available."""
         call_command("bootstrap_devsite")
