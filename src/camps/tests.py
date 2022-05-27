7ea6d028fef3841609d2183c6cef93d5dd98325d
Ramarro Marrone <rm@allmail.net>
2022-05-27 13:16:03 +0000

    fix camp menu test

--- src/camps/tests.py
+++ src/camps/tests.py
@@ -4,11 +4,11 @@ from django.test import Client, TestCase
 from django.urls import reverse
 
 
-class CampDisplayTest(TestCase):
+class CampMenuTest(TestCase):
     def test_this_year_shown_on_homepage(self):
-        """Starting in March year, current year's camp should be on the homepage."""
+        """By March, the current year's camp should be on the homepage."""
         c = Client()
-        response = c.get('/')
+        response = c.get('/news/')
         year = (datetime.date.today() - datetime.timedelta(days=59)).year
         href = reverse("camp_detail", kwargs={"camp_slug": f'bornhack-{year}'})
-        assert href in response.text
+        assert href in response.content.decode('utf-8')
