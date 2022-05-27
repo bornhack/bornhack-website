c428903eac7203e949591e2d2c778f9ad0052082
Ramarro Marrone <rm@allmail.net>
2022-05-27 13:18:30 +0000

    explain use of /news/

--- src/camps/tests.py
+++ src/camps/tests.py
@@ -8,7 +8,7 @@ class CampMenuTest(TestCase):
     def test_this_year_shown_on_homepage(self):
         """By March, the current year's camp should be on the homepage."""
         c = Client()
-        response = c.get('/news/')
+        response = c.get('/news/')  # The tests don't work with / because of the camp dispatcher.
         year = (datetime.date.today() - datetime.timedelta(days=59)).year
         href = reverse("camp_detail", kwargs={"camp_slug": f'bornhack-{year}'})
         assert href in response.content.decode('utf-8')
