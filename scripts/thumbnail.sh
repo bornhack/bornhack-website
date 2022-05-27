b94b72ad9de9f75837e7c436bc1e8153676bf357
Ramarro Marrone <rm@allmail.net>
2022-05-27 17:21:30 +0000

    simpler thumbnail script

--- scripts/thumbnail.sh
+++ scripts/thumbnail.sh
@@ -2,11 +2,8 @@
 # Convert images to thumbnail png, named with ".png" appended to the original name.
 # If the image is already a thumbnail, don't create a new one.
 
-set -e
-for image in "$@"; do
-  thumbnail_to_fullsize="$(echo "$image" | sed -e 's/^thumbnail_//' -e 's/\.[pP][nN][gG]$//')"
-  fullsize_to_thumbnail="thumbnail_$image.png"
-  if ! test -f "$thumbnail_to_fullsize" && ! test -f "$fullsize_to_thumbnail"; then
-    convert -geometry 200 "$image" "$fullsize_to_thumbnail"
-  fi
-done
+find src/static_src/img/bornhack-* \
+  -type f \
+  -and \( -iname '*.jpg' -or -iname '*.png' \) \
+  -and -not -name 'thumbnail_*' -and -not -path '*/logo/*' \
+  -execdir convert -geometry 200 "{}" "thumbnail_{}.png" \;
