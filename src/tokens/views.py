4993cf5fc12c06b5ee083c2169e3a57c0c37852c
Thomas Steen Rasmussen <tykling@bornhack.org>
2022-07-17 15:27:13 +0200

    add token metrics, fixes #1050

--- src/tokens/views.py
+++ src/tokens/views.py
@@ -5,11 +5,14 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import DetailView
 from django.views.generic import ListView
+from prometheus_client import Gauge
 
 from .models import Token
 from .models import TokenFind
 from utils.models import CampReadOnlyModeError
 
+TOKEN_FINDS = Gauge('bornhack_tokengame_token_finds_total', 'The total number of token finds by camp, user and token', ["camp", "user_id", "username", "token_id"])
+FIRST_TOKEN_FINDS = Gauge('bornhack_tokengame_first_token_finds_total', 'The tokens found for the first time', ["camp", "user_id", "username", "token_id"])
 
 class TokenFindView(LoginRequiredMixin, DetailView):
     model = Token
@@ -27,6 +30,18 @@ class TokenFindView(LoginRequiredMixin, DetailView):
             raise Http404
 
         if created:
+            # user found a new token
+            username = request.user.profile.get_public_credit_name
+            if username == "Unnamed":
+                username == "anonymous_player_{request.user.id}"
+
+            # register metrics
+            if TokenFind.objects.filter(token=self.get_object()).count() == 1:
+                # this is the first time this token has been found, count it as such
+                FIRST_TOKEN_FINDS.labels(token.camp.title, request.user.id, username, token.id).inc()
+            TOKEN_FINDS.labels(token.camp.title, request.user.id, username, token.id).inc()
+
+            # message for the user
             messages.success(
                 self.request,
                 f"Congratulations! You found a secret token: {self.get_object().description} - Your visit has been registered! Keep hunting, there might be more tokens out there.",
