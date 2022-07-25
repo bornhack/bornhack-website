870a9c0fb01d6fec599738f4094cf77d4004b6f6
Ern-st <impala.lowrider@gmail.com>
2022-07-25 13:16:55 +0200

    show correct bank details

---
+++ src/contact/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import TemplateView
+
+from django.conf import settings
+
+class ContactView(TemplateView):
+    template_name="contact.html"
+
+    def get_context_data(self, **kwargs):
+        context = {
+            "bank": settings.BANKACCOUNT_BANK,
+            "bank_iban": settings.BANKACCOUNT_IBAN,
+            "bank_bic": settings.BANKACCOUNT_SWIFTBIC,
+            "bank_dk_reg": settings.BANKACCOUNT_REG,
+            "bank_dk_accno": settings.BANKACCOUNT_ACCOUNT,
+        }
+        return context
