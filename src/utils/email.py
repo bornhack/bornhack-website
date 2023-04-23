5b782390726e7aa8137c76b4cdcaeeec62ce074f
Thomas Steen Rasmussen <tykling@bornhack.org>
2023-04-23 09:58:38 +0200

    fix invoice email sending broken in 387e962f6f5c6d4971a245dbf99944181021293c - fixes #1285

--- src/utils/email.py
+++ src/utils/email.py
@@ -31,6 +31,12 @@ def _send_email(
     if not isinstance(to_recipients, list):
         to_recipients = [to_recipients]
 
+    if not isinstance(cc_recipients, list):
+        cc_recipients = [cc_recipients]
+
+    if not isinstance(bcc_recipients, list):
+        bcc_recipients = [bcc_recipients]
+
     try:
         # put the basic email together
         msg = EmailMultiAlternatives(
