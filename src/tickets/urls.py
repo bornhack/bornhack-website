d864d0cb2ec706d9c0c250af3f57ce612132bab9
Víðir Valberg Guðmundsson <valberg@orn.li>
2023-07-23 21:45:12 +0200

    Refactor tickets list so bundles are shown seperately.

--- src/tickets/urls.py
+++ src/tickets/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
+from .views import shop_ticket_list_view
 from .views import ShopTicketDetailView
 from .views import ShopTicketDownloadView
-from .views import ShopTicketListView
 
 app_name = "tickets"
 
 urlpatterns = [
-    path("", ShopTicketListView.as_view(), name="shopticket_list"),
+    path("", shop_ticket_list_view, name="shopticket_list"),
     path(
         "<uuid:pk>/download/",
         ShopTicketDownloadView.as_view(),
