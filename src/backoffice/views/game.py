b7782d14cb4ab31163a678fdf83dd1721dd7a00a
0xUnicorn <17219001+0xUnicorn@users.noreply.github.com>
2024-07-23 12:50:27 +0200

    Added: Last token found column for 'token_stats'

--- src/backoffice/views/game.py
+++ src/backoffice/views/game.py
@@ -3,6 +3,8 @@ import logging
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.db.models import Count
+from django.db.models import Subquery
+from django.db.models import OuterRef
 from django.db.models import Q
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -99,6 +101,10 @@ class TokenStatsView(CampViewMixin, RaisePermissionRequiredMixin, ListView):
             .distinct("user")
             .values_list("user", flat=True)
         )
+        last_token_find_subquery = TokenFind.objects.filter(
+            user=OuterRef('pk'),
+            token__camp=self.camp
+        ).order_by('-created').values('created')[:1]
         return (
             User.objects.filter(id__in=tokenusers)
             .annotate(
@@ -106,6 +112,7 @@ class TokenStatsView(CampViewMixin, RaisePermissionRequiredMixin, ListView):
                     "token_finds",
                     filter=Q(token_finds__token__camp=self.camp),
                 ),
+                last_token_find=Subquery(last_token_find_subquery)
             )
             .exclude(token_find_count=0)
         )
