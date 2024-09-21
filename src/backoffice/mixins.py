b46d1f3d931e26dd5dfcbc67d918c77f9c9e30e0
Thomas Steen Rasmussen <tykling@bornhack.org>
2024-09-21 08:53:03 +0200

    make linters happy

--- src/backoffice/mixins.py
+++ src/backoffice/mixins.py
@@ -4,7 +4,6 @@ from django.shortcuts import get_object_or_404
 
 from camps.mixins import CampViewMixin
 from economy.models import Pos
-from economy.models import PosSale
 from utils.mixins import RaisePermissionRequiredMixin
 
 
