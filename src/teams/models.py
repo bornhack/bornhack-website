b7fbc96998273e1f4d680081e01d0c41d67f6787
Thomas Steen Rasmussen <tykling@bornhack.org>
2024-10-02 06:24:31 +0200

    handle cases where the perm doesn't exist yet

--- src/teams/models.py
+++ src/teams/models.py
@@ -383,10 +383,14 @@ class TeamMember(ExportModelOperationsMixin("team_member"), CampRelatedModel):
 
     def update_team_lead_permissions(self, deleted=False):
         """Ensure team lead perms for this team membership are correct."""
-        lead_perm = Permission.objects.get(
-            codename=f"{self.team.slug}_team_lead",
-            content_type=ContentType.objects.get_for_model(CampPermission),
-        )
+        try:
+            lead_perm = Permission.objects.get(
+                codename=f"{self.team.slug}_team_lead",
+                content_type=ContentType.objects.get_for_model(CampPermission),
+            )
+        except Permision.DoesNotExist:
+            logger.error(f"team lead permission not found for {self.team}")
+            return
         if self.approved and self.lead and not deleted:
             logger.debug(
                 f"Adding team lead permissions to user {self.user} for {self.team}",
