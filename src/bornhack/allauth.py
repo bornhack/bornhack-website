bab355963f50d7fe6d7e939df9dc09413894c39a
Thomas Steen Rasmussen <tykling@bornhack.org>
2024-10-06 11:13:26 +0200

    change usernames to UUID4s

---
+++ src/bornhack/allauth.py
@@ -0,0 +1,9 @@
+"""Custom allauth account adapter based on allauth_2fa.adapter.OTPAdapter."""
+from allauth_2fa.adapter import OTPAdapter
+import uuid
+
+class UsernameUUIDAdapter(OTPAdapter):
+    """Custom allauth account adapter based on allauth_2fa.adapter.OTPAdapter."""
+    def generate_unique_username(txts, regex=None):
+        """Generate a UUID4 as username."""
+        return str(uuid.uuid4())
