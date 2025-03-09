82e443c5738160dd51c0b15a4850c4f831d67566
Rudy (zarya) <git@gigafreak.net>
2025-03-09 20:18:02 +0100

    Add authenticator template, enable django.contrib.humanize

--- src/bornhack/settings.py
+++ src/bornhack/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "django.contrib.gis",
+    "django.contrib.humanize",
     "colorfield",
     "channels",
     "corsheaders",
