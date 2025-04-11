69636b2101c92f3572358e91c8df7bfff719484e
Thomas Steen Rasmussen <tykling@bornhack.org>
2025-04-11 19:37:34 +0200

    update cors url regex

--- src/bornhack/settings.py
+++ src/bornhack/settings.py
@@ -149,7 +149,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_URLS_REGEX = r"^/api/*$"
+CORS_URLS_REGEX = r"^/profile/api/$|^/o/.*$|^/bornhack-20[0-9]{2}/phonebook/json/$"
 
 if DEBUG_TOOLBAR_ENABLED:  # noqa: F405
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
