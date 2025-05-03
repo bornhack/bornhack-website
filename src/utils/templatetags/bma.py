38373fd232be50c7058bc07b9ca01faffb404987
Rudy (zarya) <git@gigafreak.net>
2025-05-03 20:36:35 +0200

    Default size should be 150

--- src/utils/templatetags/bma.py
+++ src/utils/templatetags/bma.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 @register.inclusion_tag("bma_widget.html", takes_context=True)
-def bma(context, uuid, style="splide", width=0, ratio="1/1"):
+def bma(context, uuid, style="splide", width=150, ratio="1/1"):
     count = context.get("bma_widget_counter", 0)
     context["bma_widget_counter"] = count + 1
     return {
