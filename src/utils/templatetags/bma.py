5fae78cdb40f310b94e430fd580b4eb653c306bc
Rudy <387694+zarya@users.noreply.github.com>
2025-05-09 07:07:40 +0200

    Added width and ratio to widget (#1843)
    * Added width and ratio to widget
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Default size should be 150
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Thomas Steen Rasmussen <tykling@bornhack.org>

--- src/utils/templatetags/bma.py
+++ src/utils/templatetags/bma.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 @register.inclusion_tag("bma_widget.html", takes_context=True)
-def bma(context, uuid, style="splide"):
+def bma(context, uuid, style="splide", width=150, ratio="1/1"):
     count = context.get("bma_widget_counter", 0)
     context["bma_widget_counter"] = count + 1
     return {
@@ -13,4 +13,6 @@ def bma(context, uuid, style="splide"):
         "count": count,
         "uuid": uuid,
         "style": style,
+        "width": width,
+        "ratio": ratio,
     }
