410c4e0617561c4ffbaf7508999c0aa82a0c414e
Rudy (zarya) <git@gigafreak.net>
2025-05-10 16:44:56 +0200

    Make it smaller

--- src/utils/templatetags/formatting.py
+++ src/utils/templatetags/formatting.py
@@ -6,9 +6,7 @@ register = template.Library()
 
 @register.filter(name="format_json")
 def format_json(value):
-    if isinstance(value, (dict, list, set)):
-        try:
-            return json.dumps(value, indent=2)
-        except Exception:
-            return value
-    return value
+    try:
+        return json.dumps(value, indent=2)
+    except ValueError:
+        return value
