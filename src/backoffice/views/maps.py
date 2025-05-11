7d1c12c4ca9b7ce0138f6bae9cdd37e3d553ecc3
Thomas Steen Rasmussen <tykling@bornhack.org>
2025-05-11 15:32:43 +0200

    Update src/backoffice/views/maps.py

--- src/backoffice/views/maps.py
+++ src/backoffice/views/maps.py
@@ -513,7 +513,7 @@ class MapuserLocationTypeUpdateView(MapperTeamViewMixin, UpdateView):
         return form
 
     def get_success_url(self):
-        messages.success(self.request, "The User Location Type has been created")
+        messages.success(self.request, "The User Location Type has been updated")
         return reverse(
             "backoffice:map_user_location_type_list",
             kwargs={"camp_slug": self.kwargs["camp_slug"]},
