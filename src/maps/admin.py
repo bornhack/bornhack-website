05c53d4ded3c109555d81a641e2ee30bdb811bc2
Rudy (zarya) <git@gigafreak.net>
2025-05-13 15:42:24 +0200

    Remove unused

--- src/maps/admin.py
+++ src/maps/admin.py
@@ -2,12 +2,6 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from django.db.models import QuerySet
-    from django.http import HttpRequest
-
 from typing import ClassVar
 
 from django.contrib import admin
