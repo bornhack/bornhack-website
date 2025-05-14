30a40d9fc88a5320262304bf189de5da2b1cda3f
Rudy <387694+zarya@users.noreply.github.com>
2025-05-14 07:01:19 +0200

    Token ruffing, add unit tests for tokens app (#1857)

--- src/tokens/apps.py
+++ src/tokens/apps.py
@@ -1,7 +1,11 @@
+"""All apps for token application."""
+
 from __future__ import annotations
 
 from django.apps import AppConfig
 
 
 class TokensConfig(AppConfig):
+    """TokensConfig."""
+
     name = "tokens"
--- src/tokens/urls.py
+++ src/tokens/urls.py
@@ -1,3 +1,5 @@
+"""All URLs for the Token application."""
+
 from __future__ import annotations
 
 from django.urls import path
