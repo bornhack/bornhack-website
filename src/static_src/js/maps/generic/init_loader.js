90b94145e8a5bfc986f387f17c80b08097defe0c
Rudy (zarya) <git@gigafreak.net>
2025-06-07 08:40:54 +0200

    Initial support for use my location

--- src/static_src/js/maps/generic/init_loader.js
+++ src/static_src/js/maps/generic/init_loader.js
@@ -17,3 +17,38 @@ window.addEventListener("map:init", function (event) {
     });
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  /* Method run when location lookup was successfull */
+  function location_place_success(pos) {
+    const crd = pos.coords;
+    //alert(`Your current position is: ${crd.latitude}/${crd.longitude} More or less ${crd.accuracy} meters.`)
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    const geomInput = document.getElementsByClassName('django-leaflet-raw-textarea')[0];
+    if (geomInput) {
+      geomInput.value = `{"type":"Point","coordinates":[${crd.longitude},${crd.latitude}]}`;
+      document.querySelector("form").submit();
+    } else {
+      console.log("Could not find input field");
+    }
+  }
+  function location_error(err) {
+    alert(`ERROR(${err.code}): ${err.message}`);
+  }
+  const location_options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  var use_location_button = document.getElementById("use_location"); 
+  if (use_location_button) {
+    use_location_button.addEventListener("click", function(event){
+      navigator.geolocation.getCurrentPosition(location_place_success, location_error, location_options);
+      event.preventDefault()
+    });
+  }
+});
