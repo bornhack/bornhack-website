179e6e0abb77c5009386ac3e5dab0b4b676a3929
Thomas Steen Rasmussen <tykling@bornhack.org>
2025-06-08 09:19:36 +0200

    use camps.utils.get_closest_camp() in CampRedirectView

--- src/camps/views.py
+++ src/camps/views.py
@@ -10,55 +10,15 @@ from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from .models import Camp
+from .utils import get_closest_camp
 
 logger = logging.getLogger(f"bornhack.{__name__}")
 
 
 class CampRedirectView(View):
     def dispatch(self, request, *args, **kwargs):
-        now = timezone.now()
-
-        try:
-            camp = Camp.objects.get(camp__contains=now)
-            logger.debug(
-                "Redirecting to camp '{}' for page '{}' because it is now!".format(camp.slug, kwargs["page"]),
-            )
-            return redirect(kwargs["page"], camp_slug=camp.slug)
-        except Camp.DoesNotExist:
-            pass
-
-        # no ongoing camp, find the closest camp in the past
-        try:
-            prevcamp = Camp.objects.filter(camp__endswith__lt=now).order_by("-camp").first()
-        except Camp.DoesNotExist:
-            prevcamp = None
-
-        # find the closest upcoming camp
-        try:
-            nextcamp = Camp.objects.filter(camp__startswith__gt=now).order_by("camp").first()
-        except Camp.DoesNotExist:
-            nextcamp = None
-
-        percentpassed = False
-        if prevcamp and nextcamp:
-            # find the number of days between the two camps
-            daysbetween = (nextcamp.camp.lower - prevcamp.camp.upper).days
-
-            # find the number of days since the last camp ended
-            dayssinceprevcamp = (timezone.now() - prevcamp.camp.lower).days
-
-            # find the percentage of time passed
-            percentpassed = (dayssinceprevcamp / daysbetween) * 100
-
-        # figure out where to redirect
-        if percentpassed > settings.CAMP_REDIRECT_PERCENT or not prevcamp:
-            # either we have no previous camp, or we have both and more than settings.CAMP_REDIRECT_PERCENT has passed, so redirect to the next camp
-            camp = nextcamp
-        else:
-            # either we have no next camp, or we have both and less than settings.CAMP_REDIRECT_PERCENT has passed, so redirect to the previous camp
-            camp = prevcamp
-
-        # do the redirect
+        """Find closest camp and redirect to it."""
+        camp = get_closest_camp(timezone.now())
         return redirect(kwargs["page"], camp_slug=camp.slug)
 
 
