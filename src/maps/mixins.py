a3dd2e75c1ae56d5ba085683fb181ac14e555736
Rudy <387694+zarya@users.noreply.github.com>
2025-07-06 10:59:45 +0200

    Add facilities and layers to the layer editor (#1886)

--- src/maps/mixins.py
+++ src/maps/mixins.py
@@ -5,13 +5,18 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from django.db.models import QuerySet
     from django.http import HttpRequest
 
 from django.contrib import messages
 from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
+from django.templatetags.static import static
+from django.urls import reverse
 
 from camps.mixins import CampViewMixin
+from facilities.models import FacilityType
 
 from .models import ExternalLayer
 from .models import Layer
@@ -99,3 +104,69 @@ class ExternalLayerMapperViewMixin(ExternalLayerViewMixin):
             return
         messages.error(request, "No thanks")
         raise PermissionDenied
+
+
+class LayerMapMixin:
+    """Mixin for loading the map data from the layers."""
+
+    camp_slug = None
+
+    def setup(self, request: HttpRequest, *args, **kwargs) -> None:
+        """Class init method."""
+        super().setup(request, *args, **kwargs)
+        self.camp_slug = kwargs["camp_slug"]
+
+    def get_layers(self) -> QuerySet:
+        """Method to get the layers the user has access to."""
+        user_teams = []
+        if not self.request.user.is_anonymous:
+            user_teams = self.request.user.teammember_set.filter(
+                team__camp__slug=self.camp_slug,
+            ).values_list("team__name", flat=True)
+        return Layer.objects.filter(
+            ((Q(responsible_team__camp__slug=self.camp_slug) | Q(responsible_team=None)) & Q(public=True))
+            | (Q(responsible_team__name__in=user_teams) & Q(public=False)),
+        )
+
+    def get_map_data(self) -> dict:
+        """Method to return the map_data."""
+        map_data = {
+            "grid": static("json/grid.geojson"),
+            "loggedIn": self.request.user.is_authenticated,
+            "layers": [],
+            "facilitytype_list": [],
+        }
+        facilitytype_list = FacilityType.objects.filter(
+            responsible_team__camp__slug=self.camp_slug,
+        )
+        map_data.update({"facilitytype_list": list(facilitytype_list.values())})
+        layers = self.get_layers()
+        map_data.update(
+            {
+                "layers": list(
+                    layers.values(
+                        "description",
+                        "name",
+                        "slug",
+                        "uuid",
+                        "icon",
+                        "invisible",
+                        "group__name",
+                    ),
+                ),
+            },
+        )
+        for facility in map_data["facilitytype_list"]:
+            facility["url"] = reverse(
+                "facilities:facility_list_geojson",
+                kwargs={
+                    "camp_slug": self.camp_slug,
+                    "facility_type_slug": facility["slug"],
+                },
+            )
+        for layer in map_data["layers"]:
+            layer["url"] = reverse(
+                "maps:map_layer_geojson",
+                kwargs={"layer_slug": layer["slug"]},
+            )
+        return map_data
