58b1443302ac76eb011c7bf13187750bf206c37a
Vidir Valberg Gudmundsson <valberg@orn.li>
2025-12-23 23:17:07 +0100

    Fix tests.

--- src/shop/tests.py
+++ src/shop/tests.py
@@ -689,7 +689,7 @@ class TestRefund(TestCase):
 
         # Now to refunding a bundle
         form_data = self._get_form_data(
-            refund_ticket_groups=[self.opr4.ticketgroups.order_by("created").first()],
+            refund_ticket_groups=[self.opr4.ticketgroups.latest("created")],
         )
         response = self.client.post(url, form_data)
 
@@ -714,7 +714,7 @@ class TestRefund(TestCase):
             form_data[f"ticket-{opr.id}-INITIAL_FORMS"] = "1"
             form_data[f"ticket-{opr.id}-MIN_NUM_FORMS"] = "0"
             form_data[f"ticket-{opr.id}-MAX_NUM_FORMS"] = "1000"
-            for index, ticket in enumerate(opr.shoptickets.order_by("-created").all()):
+            for index, ticket in enumerate(opr.shoptickets.order_by("-created")):
                 form_data[f"ticket-{opr.id}-{index}-uuid"] = str(ticket.uuid)
                 if ticket in refund_tickets:
                     form_data[f"ticket-{opr.id}-{index}-refund"] = "on"
@@ -724,7 +724,7 @@ class TestRefund(TestCase):
             form_data[f"ticket-group-{opr.id}-INITIAL_FORMS"] = "1"
             form_data[f"ticket-group-{opr.id}-MIN_NUM_FORMS"] = "0"
             form_data[f"ticket-group-{opr.id}-MAX_NUM_FORMS"] = "1000"
-            for index, ticket_group in enumerate(opr.ticketgroups.order_by("-created").all()):
+            for index, ticket_group in enumerate(opr.ticketgroups.order_by("-created")):
                 form_data[f"ticket-group-{opr.id}-{index}-uuid"] = str(
                     ticket_group.uuid,
                 )
