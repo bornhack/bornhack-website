e0c27aa9656d804bb4890da18a0410c3fb4a1713
Christian Henriksen <unicorn@bornhack.org>
2025-12-29 19:25:42 +0100

    Add state column and refactor processing function in admin panel

--- src/feedback/admin.py
+++ src/feedback/admin.py
@@ -8,15 +8,27 @@ from .models import Feedback
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ("user", "camp", "feedback", "processed_at", "processed_by")
+    list_display = ("user", "camp", "feedback", "state", "processed_at", "processed_by")
     list_filter = ["processed_at"]
-    readonly_fields = ["user", "camp", "feedback", "processed_at", "processed_by"]
+    readonly_fields = ["user", "camp", "feedback", "state", "processed_at", "processed_by"]
 
-    actions = ["mark_as_processed"]
+    actions = [
+        "mark_as_reviewed",
+        "mark_as_spam"
+    ]
 
-    @admin.action(description="Mark this feedback as processed")
-    def mark_as_processed(self, request, queryset) -> None:
+    @admin.action(description="Mark this feedback as reviewed")
+    def mark_as_reviewed(self, request, queryset) -> None:
         queryset.update(
             processed_at=timezone.now(),
             processed_by=request.user,
+            state="reviewed",
+        )
+
+    @admin.action(description="Mark this feedback as spam")
+    def mark_as_spam(self, request, queryset) -> None:
+        queryset.update(
+            processed_at=timezone.now(),
+            processed_by=request.user,
+            state="spam",
         )
