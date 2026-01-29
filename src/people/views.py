8df179d0e2569e3925317b581c56a41817ca03fc
Christian Henriksen <unicorn@bornhack.org>
2026-01-29 12:08:21 +0100

    Prefetch and Select related objects in the view.
    Change template to loop through member list for each column, instead of making new queries.

--- src/people/views.py
+++ src/people/views.py
@@ -1,10 +1,22 @@
 from __future__ import annotations
 
 from django.views.generic import ListView
+from django.db.models import Prefetch
 
 from camps.models import Camp
+from teams.models import TeamMember
 
 
 class PeopleView(ListView):
     template_name = "people.html"
     model = Camp
+
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related(
+            "teams",
+            Prefetch(
+                "teams__teammember_set",
+                queryset=TeamMember.objects.select_related("user__profile")
+            )
+        )
+
